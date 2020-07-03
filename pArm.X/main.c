23d58c87e253b81edb06f3d98bdb2ff5f1e57a12
Javinator9889 <dev@javinator9889.com>
2020-07-03 12:07:44 +0200

    Initial commit

---
+++ pArm.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: javinator9889
+ *
+ * Created on 11 de junio de 2020, 11:16
+ */
+
+
+#include <xc.h>
+
+#pragma config CPD = OFF, BOREN = OFF, IESO = OFF, DEBUG = OFF, FOSC = HS
+#pragma config FCMEN = OFF, MCLRE = ON, WDTE = OFF, CP = OFF, LVP = OFF
+#pragma config PWRTE = ON, BOR4V = BOR21V, WRT = OFF
+
+int main(void) {
+    int value = 0;
+
+    return 0;
+}
