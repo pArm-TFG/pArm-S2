6c18072f44712f2ef0aee42af887b984d8e1ab26
metalex121 <metalex121@gmail.com>
2020-07-31 13:12:26 +0200

    F

---
+++ pArm.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# recording configuration
+CND_ARTIFACT_DIR_recording=dist/recording/production
+CND_ARTIFACT_NAME_recording=pArm.X.production.hex
+CND_ARTIFACT_PATH_recording=dist/recording/production/pArm.X.production.hex
+CND_PACKAGE_DIR_recording=${CND_DISTDIR}/recording/package
+CND_PACKAGE_NAME_recording=parm.x.tar
+CND_PACKAGE_PATH_recording=${CND_DISTDIR}/recording/package/parm.x.tar
+# simulation configuration
+CND_ARTIFACT_DIR_simulation=dist/simulation/production
+CND_ARTIFACT_NAME_simulation=pArm.X.production.hex
+CND_ARTIFACT_PATH_simulation=dist/simulation/production/pArm.X.production.hex
+CND_PACKAGE_DIR_simulation=${CND_DISTDIR}/simulation/package
+CND_PACKAGE_NAME_simulation=parm.x.tar
+CND_PACKAGE_PATH_simulation=${CND_DISTDIR}/simulation/package/parm.x.tar
